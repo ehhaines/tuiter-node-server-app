f67a4b9d7251296a18cb4fb2555fc3a6d6c224f7
ehhaines <haines.e@northeastern.edu>
2022-11-14 18:53:11 -0500

    Complete section 4

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ import HelloController
   from "./controllers/hello-controller.js"
 import UserController
   from "./controllers/users/users-controller.js"
+
 const app = express()
+app.use(express.json());
 HelloController(app)
 UserController(app)
 app.listen(4000)
