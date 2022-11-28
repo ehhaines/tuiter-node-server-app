00b7da2db91d95bfa6958c5f9a30dc6564b4ee1e
ehhaines <haines.e@northeastern.edu>
2022-11-28 10:03:09 -0500

    Update tuits-schema to hold user information

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -1,6 +1,9 @@
 import mongoose from 'mongoose';
 const schema = mongoose.Schema({
   tuit: String,
+  username: String,
+  image: String,
+  time: String,
   likes: Number,
   liked: Boolean,
   dislikes: Number,
